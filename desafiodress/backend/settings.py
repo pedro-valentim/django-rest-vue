6cdf4554201c231557b263afc8a2c32c59e3f9b6
Pedro Valentim <pedrovalentim.leite@gmail.com>
2018-04-02 16:27:49 -0300

    Adiciona diretivas MEDIA_ROOT e MEDIA_URL no settings

--- desafiodress/backend/settings.py
+++ desafiodress/backend/settings.py
@@ -107,6 +107,9 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
